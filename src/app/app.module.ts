b55163702b8403d13b12c1936f494c875004821d
thanhvu <armormaster233@gmail.com>
2022-07-15 10:41:54 +0700

    make main-section layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
+import { MainComponent } from './components/main/main.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
-  declarations: [AppComponent, FormComponent],
+  declarations: [AppComponent, FormComponent, MainComponent, CounterComponent, FilterComponent, ListItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
