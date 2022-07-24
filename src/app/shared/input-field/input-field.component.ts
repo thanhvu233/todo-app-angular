13ce064623d24afd9333ca964c845ae156c36128
thanhvu <armormaster233@gmail.com>
2022-07-24 15:59:47 +0700

    create input-field component

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.scss']
+  styleUrls: ['./input-field.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputFieldComponent,
+      multi: true,
+    },
+  ],
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent implements OnInit, ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() hint?: string = '';
+  @Input() type: string = 'text';
+  @Input() isInvalid: boolean | undefined = false;
+  value: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  writeValue(obj: string): void {
+    this.value = obj;
   }
 
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  ngOnInit(): void {}
+
+  onChange(value: string): void {}
+  onTouched(): void {}
 }
